cd097e545f9c0ba8bfa9507c7cbb4062369ed7f2
Rayyaki <rezky24.afrianata@gmail.com>
2023-03-06 14:52:18 +0700

    exer5

--- fundamental/Day6/note.js
+++ fundamental/Day6/note.js
@@ -17,17 +17,55 @@ let user = {
 
 
 
+// add and delete
+
+let siswa = {
+    nama : 'Rayyaki',
+    kelas : 'JCWD',
+    
+}
+
+// cara menambahkan prpperty kedalam object
+
+siswa.hobby = 'coding'
+console.log(siswa);
+
+siswa.age = 30
+console.log(siswa);
+
+// cara mendelete property pada object
+
+
+
+
+
+
+//
+
+let person = {
+    fname: "Rizki",
+    lname: "Afrianata",
+    alname : function () {
+        console.log( `My name is  : ${this.fname} ${this.lname}`);
+    }
+}
+
+person.alname()
+
 
 
 
 // for..in loop
 
-let person = {
-    name  : 'Rizki',
-    age   : 30,
-}
+// let person = {
+//     name  : 'Rizki',
+//     age   : 30,
+// }
 
-for (let key in person){
-    console.log(key);
-    console.log(person[key]);
-}
+// for (let key in person){
+//     console.log(key);
+//     console.log(person[key]);
+// }
+
+
+// // 
